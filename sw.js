a4631dbecab772b786a6ccdcafc41ce9ca0d2282
Habib2959 <rahman.habibjr59@gmail.com>
2024-11-26 14:39:12 +0600

    version changed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //service worker
-const VERSION = "v1";
+const VERSION = "v2";
 const CACHE_NAME = `pwa-${VERSION}`;
 const APP_STATIC_RESOURCES = [
 	"/",
@@ -32,27 +32,4 @@ self.addEventListener("activate", (event) => {
 	);
 });
 
-// On fetch, intercept server requests
-// and respond with cached responses instead of going to network
-self.addEventListener("fetch", (event) => {
-	// As a single page app, direct app to always go to cached home page.
-	if (event.request.mode === "navigate") {
-		event.respondWith(caches.match("/"));
-		return;
-	}
-
-	// For all other requests, go to the cache first, and then the network.
-	event.respondWith(
-		(async () => {
-			const cache = await caches.open(CACHE_NAME);
-			const cachedResponse = await cache.match(event.request.url);
-			if (cachedResponse) {
-				// Return the cached response if it's available.
-				return cachedResponse;
-			}
-			// If resource isn't in the cache, return a 404.
-			return new Response(null, { status: 404 });
-		})()
-	);
-});
 // End of snippet
